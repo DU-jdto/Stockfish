9cb6bf4bb975c26f09095985970bbba52fdd9882
DU-jdto <jadto@outlook.com>
2018-03-07 00:22:31 +1100

    Reductions simplification

--- src/search.cpp
+++ src/search.cpp
@@ -155,12 +155,8 @@ void Search::init() {
           {
               double r = log(d) * log(mc) / 1.95;
 
-              Reductions[NonPV][imp][d][mc] = int(std::round(r));
-              Reductions[PV][imp][d][mc] = std::max(Reductions[NonPV][imp][d][mc] - 1, 0);
-
-              // Increase reduction for non-PV nodes when eval is not improving
-              if (!imp && Reductions[NonPV][imp][d][mc] >= 2)
-                Reductions[NonPV][imp][d][mc]++;
+              Reductions[NonPV][imp][d][mc] = int(std::round(r + 0.5 * !imp));
+              Reductions[PV][imp][d][mc] = std::max(int(std::round(r)) - 1, 0);
           }
 
   for (int d = 0; d < 16; ++d)
