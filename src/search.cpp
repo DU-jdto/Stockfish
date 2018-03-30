79898b7e0e9b87cb2288182cf57b88965f1e2d37
DU-jdto <jadto@outlook.com>
2018-03-31 04:36:24 +1100

    Take 3

--- src/search.cpp
+++ src/search.cpp
@@ -891,7 +891,8 @@ moves_loop: // When in check, search starts from here
               && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= Value(5000)))
           {
               // Move count based pruning
-              if (moveCountPruning)
+              if (   moveCountPruning
+                  && !mp.is_refutation(move))
               {
                   skipQuiets = true;
                   continue;
@@ -902,6 +903,7 @@ moves_loop: // When in check, search starts from here
 
               // Countermoves based pruning
               if (   lmrDepth < 3
+                  && move != countermove
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
