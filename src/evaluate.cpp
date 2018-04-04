939106008eb31ef45ce0973dd2c06c9b163a8037
DU-jdto <jadto@outlook.com>
2018-04-04 11:10:21 +1000

    Only do lazy eval when both MG and EG are above the threshold

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -848,7 +848,7 @@ namespace {
 
     // Early exit if score is high
     Value v = (mg_value(score) + eg_value(score)) / 2;
-    if (abs(v) > LazyThreshold)
+    if (abs(mg_value(score)) > LazyThreshold && abs(eg_value(score)) > LazyThreshold)
        return pos.side_to_move() == WHITE ? v : -v;
 
     // Main evaluation begins here
