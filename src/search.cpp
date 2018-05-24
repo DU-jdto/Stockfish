4bfcff6fdf9da98e021ec7c153b55c4f734cde84
DU-jdto <jadto@outlook.com>
2018-05-25 05:03:29 +1000

    Change the threshold for extra capture reduction

--- src/search.cpp
+++ src/search.cpp
@@ -998,7 +998,7 @@ moves_loop: // When in check, search starts from here
           {
               //Increase reduction by comparing opponent's stat score
               if (   (ss-1)->statScore >= 0 
-                  && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 0)
+                  && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < -PieceValue[EG][pos.captured_piece()])
                   r += ONE_PLY;
                             
               r -= r ? ONE_PLY : DEPTH_ZERO;
